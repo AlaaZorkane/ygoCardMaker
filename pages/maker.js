98ec3016cb0558d42a2c23dcc9365ec33540636c
AlaaZorkane <raralazoril@gmail.com>
2018-05-02 17:06:21 +0100

    init commit

---
+++ pages/maker.js
@@ -0,0 +1,27 @@
+define(["react", "react-class",], 
+
+///////////////////////////////////////////////////////////////////
+
+function App(React, ReactClass, Card,) {
+    var emptyCard = { 
+        version: "0.0.1a",
+        rarity: "Common",
+        name: "",
+        level: 0,
+        type: "",
+        effect: "",
+        atk: "",
+        def: "",
+        serial: "Serial nibba",
+        copyright: "© 2018 Alae Zorkane",
+        attribute: "None",
+        pendulum: 
+        {
+            enabled: false,
+            effect: "",
+            blue: "5",
+            red: "5"
+        },
+        layout: "Normal"
+    }
+});
